887b8a62d6da4ae95837eb36ef18b38d70f2c0d3
correalo <82238324+correalo@users.noreply.github.com>
2022-02-03 20:37:46 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express/multer/interceptors';
+import { AppService } from './app.service';
+
+class Teste {
+  message: string;
+}
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Body() body: Teste) {
+    console.log(file);
+    console.log(body);
+  }
+}
